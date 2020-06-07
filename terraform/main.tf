d17b3740c4346cec2a4cced8832b166a25770cce
Ujjwal Bagve <ujjwal.bagwe@here.com>
2020-06-07 15:25:20 +0530

    Auto approval added for Terrform apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
+    resource_group_name  = "tstate"
     storage_account_name = "tstate31493"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
